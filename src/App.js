297ffca344b8ddc3e3994dd52673ad6b3248d9cd
MathieuGAUBAL <b_jayson@hotmail.com>
2022-03-22 16:00:39 +0100

    responsive tablet ok

--- src/App.js
+++ src/App.js
@@ -3,18 +3,23 @@ import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
 import UnderCard from './components/under_card/UnderCard';
+import Image from './components/_shared/Image';
 import './scss/styles.css';
 
 function App() {
   return (
     <div className='bkgrnd'>
+      <Image path={"/assets/bg-pattern-1.svg"} alt={" "} cln={"illustration1-nav"} />
       <div className='wrapper'>
         <div className='section1'>
           <Nav />
           <Header />
         </div>
-        <UnderCard />
-        <Card />
+        <div className='section2'>
+          <UnderCard />
+          <Card />
+        </div>
+
         <Footer />
       </div>
     </div>
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,14 +3,17 @@ import Image from "../_shared/Image";
 const Footer = () => {
     return (
         <footer className="container-footer">
-            <p>equalizer</p>
-            <p>All rights reserved © Equalizer 2021
-Have any problems? Contact us via social media or email us at <b>equalizer@example.com</b></p>
-            <ul>
-                <li><Image path="/assets/icon-facebook.svg" alt="logo facebook" cln="facebook"/></li>
-                <li><Image path="/assets/icon-instagram.svg" alt="logo instagram" cln="instagram"/></li>
-                <li><Image path="/assets/icon-twitter.svg" alt="logo twitter" cln="twitter"/></li>
-            </ul>
+            <h2>equalizer</h2>
+            <div className="footer-content">
+                <p>All rights reserved © Equalizer 2021
+                    Have any problems? Contact us via social media or email us at <b>equalizer@example.com</b></p>
+                <ul>
+                    <li><Image path="/assets/icon-facebook.svg" alt="logo facebook" cln="facebook" /></li>
+                    <li><Image path="/assets/icon-instagram.svg" alt="logo instagram" cln="instagram" /></li>
+                    <li><Image path="/assets/icon-twitter.svg" alt="logo twitter" cln="twitter" /></li>
+                </ul>
+            </div>
+
         </footer>
     )
 }

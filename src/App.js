9df900b8852489010e53f0f8173c75e65decf932
MathieuGAUBAL <b_jayson@hotmail.com>
2022-03-22 11:21:01 +0100

    fix app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from './components/card/Card';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import UnderCard from './components/under_card/UnderCard';
+import './scss/styles.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bkgrnd'>
+      <div className='wrapper'>
+        <div className='section1'>
+          <Nav />
+          <Header />
+        </div>
+        <UnderCard />
+        <Card />
+        <Footer />
+      </div>
     </div>
+
   );
 }
 

40de237dbcf92c18738c1a33f3b0e82f3c83abd8
MathieuGAUBAL <b_jayson@hotmail.com>
2022-03-23 21:24:03 +0100

    fix css

--- src/components/under_card/UnderCard.js
+++ src/components/under_card/UnderCard.js
@@ -4,9 +4,12 @@ import Image from "../_shared/Image";
 const UnderCard = ({ children }) => {
     return (
         <div className="section2">
-            <Image path="/assets/bg-pattern-2.svg" alt="bg-pattern-1.svg" cln="undercard" />
-            <Image path="/assets/illustration-app.png" alt="illustration-app.png" cln="phone" />
-            {children}
+            <div className="main-undercard">
+                <Image path="assets/bg-pattern-2.svg" alt="bg-pattern-1.svg" cln="undercard" />
+                <Image path="assets/illustration-app.png" alt="illustration-app.png" cln="phone" />
+                {children}
+
+            </div>
         </div>
 
     )

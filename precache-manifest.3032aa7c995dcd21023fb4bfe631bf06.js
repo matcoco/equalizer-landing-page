7d34356c225ebb832d6febc33a5435bcf9ed2852
MathieuGAUBAL <b_jayson@hotmail.com>
2022-03-23 20:49:55 +0100

    Updates

--- precache-manifest.3032aa7c995dcd21023fb4bfe631bf06.js
+++ precache-manifest.3032aa7c995dcd21023fb4bfe631bf06.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "588fd3a930791b960e83",
-    "url": "/equalizer-landing-page/static/css/main.2987a0ec.chunk.css"
+    "revision": "999f6f942292a1e0ef6b",
+    "url": "/equalizer-landing-page/static/css/main.5cde1c4f.chunk.css"
   },
   {
-    "revision": "588fd3a930791b960e83",
-    "url": "/equalizer-landing-page/static/js/main.588fd3a9.chunk.js"
+    "revision": "999f6f942292a1e0ef6b",
+    "url": "/equalizer-landing-page/static/js/main.999f6f94.chunk.js"
   },
   {
     "revision": "521dee7d6a1a5478516b",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/equalizer-landing-page/static/js/runtime~main.26a6711e.js"
   },
   {
-    "revision": "bbb6a3eb0a1e05499004adcafe8b3073",
+    "revision": "46f983c719b9fd1673b9cce61890b86e",
     "url": "/equalizer-landing-page/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/equalizer-landing-page/precache-manifest.2b75b4a69a9ae5d781e4acd60a6bd6a6.js"
+  "/equalizer-landing-page/precache-manifest.3032aa7c995dcd21023fb4bfe631bf06.js"
 );
 
 workbox.clientsClaim();

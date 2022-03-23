1d6d1e36d86058a6f8a74e0648aecde37b78632b
MathieuGAUBAL <b_jayson@hotmail.com>
2022-03-23 16:13:02 +0100

    fix css responsive

--- src/components/under_card/UnderCard.js
+++ src/components/under_card/UnderCard.js
@@ -1,11 +1,12 @@
 import Image from "../_shared/Image";
 
-const UnderCard = () => {
+const UnderCard = ({ children }) => {
     return (
-        <>
+        <div className="section2">
             <Image path="/assets/bg-pattern-2.svg" alt="bg-pattern-1.svg" cln="undercard" />
             <Image path="/assets/illustration-app.png" alt="illustration-app.png" cln="phone" />
-        </>
+            {children}
+        </div>
 
     )
 }
